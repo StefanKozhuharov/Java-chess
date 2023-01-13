1e43d3447befc83350e36f69fa4af7596fb50c1b
StefanKozhuharov <107200328+StefanKozhuharov@users.noreply.github.com>
2023-01-13 13:21:01 +0200

    Fleshed out the Player classes

--- src/java_chess/player/Player.java
+++ src/java_chess/player/Player.java
@@ -5,6 +5,7 @@
 package java_chess.player;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -28,7 +29,7 @@ public abstract class Player {
     Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves) {
         this.board = board;
         this.playerKing = establishKing();
-        this.legalMoves = legalMoves;
+        this.legalMoves = ImmutableList.copyOf(Iterables.concat(legalMoves, calculateKingCastles(legalMoves, opponentMoves)));
         this.isInCheck = !Player.calculateAttacksOnTile(this.playerKing.getPiecePosition(), opponentMoves).isEmpty();
     }
 
@@ -40,7 +41,7 @@ public abstract class Player {
         return this.legalMoves;
     }
 
-    private static Collection<Move> calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
+    protected static Collection<Move> calculateAttacksOnTile(int piecePosition, Collection<Move> moves) {
         final List<Move> attackMoves = new ArrayList<>();
         for (final Move move : moves) {
             if (piecePosition == move.getDestinationCoordinate()) {
@@ -112,4 +113,6 @@ public abstract class Player {
     public abstract Color getColor();
 
     public abstract Player getOpponent();
+    
+    protected abstract Collection<Move> calculateKingCastles(Collection<Move> playerLegals, Collection<Move> opponentLegals);
 }
--- src/java_chess/player/WhitePlayer.java
+++ src/java_chess/player/WhitePlayer.java
@@ -4,11 +4,18 @@
  */
 package java_chess.player;
 
+import com.google.common.collect.ImmutableList;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java_chess.Color;
 import java_chess.board.Board;
 import java_chess.board.Move;
+import java_chess.board.Move.CastleMove.KingSideCastleMove;
+import java_chess.board.Move.CastleMove.QueenSideCastleMove;
+import java_chess.board.Tile;
 import java_chess.pieces.Piece;
+import java_chess.pieces.Rook;
 
 /**
  *
@@ -31,9 +38,44 @@ public class WhitePlayer extends Player {
     public Color getColor() {
         return Color.WHITE;
     }
-    
+
     @Override
     public Player getOpponent() {
         return this.board.blackPlayer();
+    }
+
+    @Override
+    protected Collection<Move> calculateKingCastles(final Collection<Move> playerLegals, final Collection<Move> opponentLegals) {
+
+        final List<Move> kingCastles = new ArrayList<>();
+
+        if (this.playerKing.isFirstMove() && !isInCheck()) {
+            //king side castle
+            if (!this.board.getTile(61).isTileOccupied() && !this.board.getTile(62).isTileOccupied()) {
+                final Tile rookTile = this.board.getTile(63);
+
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    if (Player.calculateAttacksOnTile(61, opponentLegals).isEmpty()
+                            && Player.calculateAttacksOnTile(62, opponentLegals).isEmpty()
+                            && rookTile.getPiece().getPieceType().isRook()) {
+                        kingCastles.add(new KingSideCastleMove(this.board, this.playerKing, 62, (Rook)rookTile.getPiece(), rookTile.getTileCoordinate(), 61));
+                    }
+
+                }
+            }
+
+            //queen side castle
+            if (!this.board.getTile(59).isTileOccupied()
+                    && !this.board.getTile(58).isTileOccupied()
+                    && !this.board.getTile(57).isTileOccupied()) {
+
+                final Tile rookTile = this.board.getTile(56);
+                if (rookTile.isTileOccupied() && rookTile.getPiece().isFirstMove()) {
+                    kingCastles.add(new QueenSideCastleMove(this.board, this.playerKing, 58, (Rook)rookTile.getPiece(), rookTile.getTileCoordinate(), 59));
+                }
+            }
+        }
+
+        return ImmutableList.copyOf(kingCastles);
     }
 }
